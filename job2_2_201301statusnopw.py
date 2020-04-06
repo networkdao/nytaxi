869fd7aa197d8641c9bfa2edde21282e4028812e
networkdao <networkdao@163.com>
2020-04-06 21:15:17 +0800

    job2spark to mysql
    job2spark to mysql

---
+++ job2_2_201301statusnopw.py
@@ -0,0 +1,47 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+def get_status(value):
+    if value <= 4 and value >= 0:
+        return "悠闲"
+    elif value <= 6:
+        return "轻松"
+    elif value <= 8:
+        return "饱和"
+    elif value <= 10:
+        return "疲劳"
+    elif value <= 12:
+        return "超负荷"
+    elif value >12:
+        return "严重超负荷"
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    spark = SparkSession.builder.appName("nytaxi201301status").getOrCreate()
+    #从HDFS 加载CSV 文件， 并且使用现有csv文件的表头，自动推导字段的属性， 并且选择（select）后的内容
+    data201301 = spark.read.format("csv").option("header","true").option("inferSchema","true").load("/data/hdfsfile/taxiny/trip_data_1.csv")\
+    .select("medallion","hack_license","vendor_id","rate_code","store_and_fwd_flag","pickup_datetime","dropoff_datetime","passenger_count",\
+        "trip_time_in_secs","trip_distance","pickup_longitude","pickup_latitude","dropoff_longitude","dropoff_latitude")
+    # 创建临时表
+    data201301.createOrReplaceTempView("tb201301")
+    
+    #数据清洗，去除掉行程时间小于120s 的和距离小于0.1 英里的,把清洗的数据生产一张新的虚拟表， 也可以把这个表的内容写入Hive 作为DW 的 TWD 层
+    sqlDF201301_0 = spark.sql("SELECT * FROM tb201301 WHERE trip_time_in_secs>=120 and trip_distance>=0.1") 
+    # 创建临时表
+    data201301.createOrReplaceTempView("tb201301_2")
+    
+    # spark sql 中的 TO_DATE 等于SQL 中的 DATE_FORMAT,qlDF201301_1 这个是中间结果1，表示的是每辆车每天拉客的累积时间,可以写入Hive
+    sqlDF201301_1 = spark.sql("SELECT hack_license, TO_DATE(pickup_datetime,'%Y%m%d') AS day,sum(trip_time_in_secs) AS totaltime\
+        FROM tb201301_2 GROUP BY hack_license, TO_DATE(pickup_datetime,'%Y%m%d')") 
+    
+    status_function_udf = udf(get_status,StringType())
+    # 包含新增的status 列的dataframe
+    sqlDF201301_2 = sqlDF201301_1.withColumn("status", status_function_udf((sqlDF201301_1['totaltime'])/3600))
+
+    #把最终的结果写入到mysql
+    sqlDF201301_2.write.format('jdbc').options(url='jdbc:mysql://172.20.x.x:3306/nytaxi?useUnicode=true&characterEncoding=utf8', useSSL="false",\
+driver='com.mysql.jdbc.Driver',dbtable='nytaxistaus201301',user='user1',password='********').mode('append').save()
+ 
+    spark.stop()
